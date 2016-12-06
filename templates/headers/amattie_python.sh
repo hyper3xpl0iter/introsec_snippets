da07b8ddf792b0ade62bb8b4426a082cf55fb95b
Andre Mattie <amattie@introsec.ca>
2016-12-06 07:11:33 -0500

    Added header templates

---
+++ templates/headers/amattie_python.sh
@@ -0,0 +1,13 @@
+#!/usr/env/python
+
+################################################################################
+#
+#    filename: 
+# description: 
+#       email: amattie@introsec.ca
+#     website: https://th3c1inic.org
+#         GPG: 4375 A9A8 A7B0 BCBC 44BD  DB42 4761 C4EF 2A01 1C69
+#     bitcoin: 13xSeXftdyJWXo6vsQ2pUvBEqPFn9q8o79
+#        date: 
+#
+################################################################################
---
+++ templates/headers/introsec_bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+################################################################################
+#
+#    filename:                    
+# description:                                                         
+#      author: hyper3xpl0iter
+#       email: devel@introsec.ca
+#         GPG: 4375 A9A8 A7B0 BCBC 44BD  DB42 4761 C4EF 2A01 1C69
+#     bitcoin: 1LHsfZrES8DksJ41JAXULimLJjUZJf7Qns
+#        date:           
+#
+################################################################################
+
